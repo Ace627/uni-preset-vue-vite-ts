3a9236fee1baab296342e7b19e6cab61ff8a6eb4
当时只道是寻常 <1207588603@qq.com>
2025-04-11 23:56:57 +0800

    feat：原生 wx 接口的类型

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -26,6 +26,18 @@ request.interceptors.response.use(
     return response.data
   },
   (error) => {
+    let { message } = error
+
+    if (message == 'Network Error') {
+      message = '后端接口连接异常'
+    } else if (message.includes('timeout')) {
+      message = '系统接口请求超时'
+    } else if (message.includes('Request failed with status code')) {
+      message = `系统接口 ${message.substr(message.length - 3)} 异常`
+    }
+
+    uni.showModal({ title: '请求失败', content: message, showCancel: false })
+
     return Promise.reject(error)
   },
 )
